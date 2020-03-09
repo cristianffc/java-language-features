735a38c58b570e5edb81e2fb33fa56d47ee1afe4
Cristian Fernando Flores Castañeda <cristianffc@gmail.com>
2020-03-08 23:49:13 -0300

    :sparkles: Add new consumer functional interface

--- src/main/java/lambda/ConsumerInterface.java
+++ src/main/java/lambda/ConsumerInterface.java
@@ -1,4 +1,25 @@
 package lambda;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.LongConsumer;
+
 public class ConsumerInterface {
+
+    public static void main(String[] args) {
+        Consumer<Integer> printConsole = (Integer number) -> System.out.println(number);
+        printValues(Arrays.asList(1,2,3,4,5,6), printConsole);
+
+        LongConsumer printLongValue = (long number) -> System.out.println(number);
+        printLongValue.accept(100l);
+    }
+
+    public static <T> void printValues(List<T> list, Consumer<T> consumer) {
+        List<T> results = new ArrayList();
+        for (T t : list) {
+            consumer.accept(t);
+        }
+    }
 }
