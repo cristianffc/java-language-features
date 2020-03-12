069abc1b74ebd427e813fadc60e695a89807a3fc
Cristian Fernando Flores Castañeda <cristianffc@gmail.com>
2020-03-12 19:51:11 -0300

    :sparkles: Add new method reference

--- src/main/java/methodreference/MethodReference.java
+++ src/main/java/methodreference/MethodReference.java
@@ -1,12 +1,37 @@
 package methodreference;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
+import java.util.function.ToLongFunction;
+
 public class MethodReference {
 
-    public static void Main(String[] args) {
+    public static void main(String[] args) {
 
-        Integer value = 10;
-        System.out::println(Integer::parseInt);
+        //Static method
+        ToLongFunction<String> stringToLong =
+                (String s) -> Integer.parseInt(s);
+        ToLongFunction<String> stringToLongMethodReference = Integer::parseInt;
+        System.out.println(stringToLongMethodReference.applyAsLong("100"));
 
+        //Arbitrary type
+        BiPredicate<List<String>, String> contains =
+                (list, element) -> list.contains(element);
+        BiPredicate<List<String>, String> containsMethodReference = List::contains;
+        System.out.println(containsMethodReference.test(Arrays.asList("hello","world"), "hello"));
 
+        //Instance method of an existing object or expression
+        MethodReference methodReference = new MethodReference();
+        Function<Integer, String> statement =
+            (value) -> methodReference.printText(value);
+        Function<Integer, String> statementMethodReference = methodReference::printText;
+        System.out.println(statementMethodReference.apply(10));
+
+    }
+
+    String printText(Integer value) {
+        return String.valueOf(value);
     }
 }
