104a17a395fba4332e9d7891dab25bdbd77aed2b
Cristian Fernando Flores Castañeda <cristianffc@gmail.com>
2020-05-07 20:41:18 -0300

    :sparkles: New classes related to Collectors

---
+++ src/main/java/collectors/reducingsummarize/Counter.java
@@ -0,0 +1,28 @@
+package collectors.reducingsummarize;
+
+import entity.Car;
+import entity.Color;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Counter {
+    public static void main(String[] args) {
+        Car car1 = new Car.Builder().withName("mock Car First").withColor(Color.BLUE).withSpeed(100).
+                withaAutomaticGearbox(true).
+                build();
+
+        Car car2 = new Car.Builder().withName("mock Car Second").withColor(Color.RED).withSpeed(120).
+                withaAutomaticGearbox(false).
+                build();
+
+        Car car3 = new Car.Builder().withName("mock Car Third"). withColor(Color.BLACK).withSpeed(130).
+                withaAutomaticGearbox(true).
+                build();
+
+        List<Car> cars = Arrays.asList(car1, car2, car3);
+        System.out.println(cars.stream().collect(Collectors.counting()));
+        System.out.println(cars.stream().count());
+    }
+}
